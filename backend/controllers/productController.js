1c43881e50a90102b7c95cf861229c11f15fbe57
jacksonbeaman <jacksonbeaman@gmail.com>
2021-03-29 12:29:31 -0700

    Create product controllers

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import asyncHandler from 'express-async-handler';
+// error handler so we don't need a trycatch in all of our database calls in all of our routes
+import Product from '../models/productModel.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public - no token necessary - i.e. you don't need to be logged in
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}); // passing in an empty object to the find method from mongoose gives us everything
+  res.json(products); // res.send or res.json will convert products to the JSON content type
+});
+
+// @desc    Fetch single product by id
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import Product from '../models/productModel.js';
-// error handler so we don't need a trycatch in all of our database calls in all of our routes
-import asyncHandler from 'express-async-handler';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public - no token necessary - i.e. you don't need to be logged in
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({}); // passing in an empty object to the find method from mongoose gives us everything
-    res.json(products); // res.send or res.json will convert products to the JSON content type
-  })
-);
+// router.get('/', getProducts); // also works
+router.route('/').get(getProducts);
 
-// @desc    Fetch single product by id
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+// router.get('/:id', getProductById);
+router.route('/:id').get(getProductById);
 
 export default router;
