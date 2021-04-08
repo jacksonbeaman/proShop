53df797d76485c11d3b398b9060db2401cf3a49c
jacksonbeaman <jacksonbeaman@gmail.com>
2021-04-08 14:45:58 -0700

    ShippingScreen component & save address

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 // Actions
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstants';
 
 // getState method comes from store.js / Redux
 // getState allows us to get our entire state tree, including productList, productDetails, cart, etc ...
@@ -31,4 +35,13 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   });
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  });
+
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
 };
--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,3 +1,5 @@
 // start naming of const with RESOURCE_ACTION_ETC
 export const CART_ADD_ITEM = 'CART_ADD_ITEM';
 export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM';
+
+export const CART_SAVE_SHIPPING_ADDRESS = 'CART_SAVE_SHIPPING_ADDRESS';
