f04362ab04b7ea9be50992da78bcae27c39f523d
jacksonbeaman <jacksonbeaman@gmail.com>
2021-04-21 15:01:40 -0700

    updateOrderToPaid endpont - api/orders/:id/pay

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -62,4 +62,32 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc    Update order to paid
+// @route   PUT /api/orders/:id/pay
+// @access  Private / protected
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  // set properties in order object
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    // paymentResult comes from PayPal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    // save order in database
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
