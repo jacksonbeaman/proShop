a752dca8416b1d8bf4f0ced889d0049c8da8a080
jacksonbeaman <jacksonbeaman@gmail.com>
2021-05-26 15:32:05 -0700

    Fixed userInfo state upon update user profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -160,6 +160,11 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     const { data } = await axios.put('/api/users/profile', user, config);
 
     dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
+
+    // the server returns the same payload for both USER_UPDATE_PROFILE_SUCCESS and USER_LOGIN_SUCCESS
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
